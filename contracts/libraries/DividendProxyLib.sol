ca03de5b10454b3073f1baa5eafd7c483b35fc47
David Van Isacker <davidvanisacker@gmail.com>
2018-02-01 01:10:19 +0900

    added dividend proxy contract

---
+++ contracts/libraries/DividendProxyLib.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.4.18;
+
+import "../interfaces/StoreInterface.sol";
+
+
+library DividendProxyLib {
+
+  /**
+  * @notice Dividends functions can only be called through the proof network
+  * @notice In its current form, the proof network consists of the Cryptofiat and the AMP Manager contracts
+  * @dev Implement the onlyAuthorized function when writing the ownership model
+   */
+  modifier onlyAuthorized() {
+    _;
+  }
+
+  //getters
+  function getCurrentPoolIndex(address _store) public view returns(uint256) {
+    return StoreInterface(_store).getUint(keccak256("dividends.currentPoolIndex"));
+  }
+
+  function getCurrentEpoch(address _store) public view returns(uint256) {
+    return StoreInterface(_store).getUint(keccak256("dividends.currentPoolEpoch"));
+  }
+
+  function getCurrentPoolBalance(address _store) public view returns(uint256) {
+    return StoreInterface(_store).getUint(keccak256("dividends.currentPoolBalance"));
+  }
+
+  function getNthPoolDividends(address _store, uint256 _index) public view returns(uint256) {
+    return StoreInterface(_store).getUint(keccak256("dividends.nthPoolBalance", _index));
+  }
+
+  function getNthPoolRemainingDividends(address _store, uint256 _index) public view returns(uint256) {
+    return StoreInterface(_store).getUint(keccak256("dividends.nthPoolRemainingBalance", _index));
+  }
+
+  function getAccountLastWithdrawal(address _store, address _account) public view returns(uint256) {
+    return StoreInterface(_store).getUint(keccak256("dividends.account.lastWithdrawal", _account));
+  }
+
+
+
+  //setters
+  function setCurrentPoolIndex(address _store, uint256 _value) public {
+    StoreInterface(_store).setUint(keccak256("dividends.currentPoolIndex"), _value);
+  }
+
+  function setCurrentPoolEpoch(address _store, uint256 _value) public {
+    StoreInterface(_store).setUint(keccak256("dividends.currentPoolEpoch"), _value);
+  }
+
+  function setCurrentPoolBalance(address _store, uint256 _value) public {
+    StoreInterface(_store).setUint(keccak256("dividends.currentPoolBalance"), _value);
+  }
+
+  function setNthPoolDividends(address _store, uint256 _index, uint256 _value) public {
+    StoreInterface(_store).setUint(keccak256("dividends.nthPoolBalance", _index), _value);
+  }
+
+  function setNthPoolRemainingDividends(address _store, uint256 _index, uint256 _value) public {
+    StoreInterface(_store).setUint(keccak256("dividends.nthPoolRemainingBalance", _index), _value);
+  }
+
+  function setAccountLastWithdrawal(address _store, address _account, uint256 _value) public {
+    StoreInterface(_store).setUint(keccak256("dividends.accounts.withdrawal", _account), _value);
+  }
+
+
+  //helpers
+  function incrementCurrentPoolIndex(address _store, uint256 _amount) public {
+    uint256 newValue = StoreInterface(_store).getUint(keccak256("dividends.currentPoolIndex")) + _amount;
+    StoreInterface(_store).setUint(keccak256("dividends.currentPoolIndex"), newValue);
+  }
+
+  function incrementCurrentEpoch(address _store, uint256 _amount) public {
+    uint256 newValue = StoreInterface(_store).getUint(keccak256("dividends.currentEpoch")) + _amount;
+    StoreInterface(_store).setUint(keccak256("dividends.currentEpoch"), newValue);
+  }
+
+  function incrementCurrentPoolBalance(address _store, uint256 _amount) public {
+    uint256 newValue = StoreInterface(_store).getUint(keccak256("dividends.currentPoolBalance")) + _amount;
+    StoreInterface(_store).setUint(keccak256("dividends.currentPoolBalance"), newValue);
+  }
+}
