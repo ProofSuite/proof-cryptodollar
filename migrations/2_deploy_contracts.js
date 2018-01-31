323654b15a839ff0c95099ce51354b8744e9abcb
David Van Isacker <davidvanisacker@gmail.com>
2018-02-01 01:04:35 +0900

    added basic store and proxy lib deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
-
+var DividendProxyLib = artifacts.require('./libraries/DividendProxyLib.sol')
 var Store = artifacts.require("./Store.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Store);
+module.exports = async (deployer) => {
+  await deployer.deploy(Store)
+  await deployer.deploy(DividendProxyLib)
+  deployer.link(DividendProxyLib, Store);
 };
