14b71db47fe3c7fb94c4cd8d94244183ee32159f
David Van Isacker <davidvanisacker@gmail.com>
2018-02-07 02:28:33 +0900

    [#154904445] Modified deployment process

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var DividendStorageProxy = artifacts.require('./libraries/DividendStorageProxy.sol')
+var RewardsStorageProxy = artifacts.require('./libraries/RewardsStorageProxy.sol')
 var CryptoFiatStorageProxy = artifacts.require('./libraries/CryptoFiatStorageProxy.sol')
 var CryptoDollarStorageProxy = artifacts.require('./libraries/CryptoDollarStorageProxy.sol')
 var SafeMath = artifacts.require('./libraries/SafeMath.sol')
@@ -6,24 +6,26 @@ var CryptoDollar = artifacts.require('./CryptoDollar.sol')
 var CryptoFiatHub = artifacts.require('./CryptoFiatHub.sol')
 var ProofToken = artifacts.require('./ProofToken.sol')
 var Store = artifacts.require("./Store.sol");
+var ProofRewards = artifacts.require("./ProofRewards.sol")
 
 
 //for some reason async/await makes this file crash
 module.exports = function(deployer) {
 
   deployer.deploy(SafeMath)
-  deployer.deploy(DividendStorageProxy)
+  deployer.deploy(RewardsStorageProxy)
   deployer.deploy(CryptoFiatStorageProxy)
   deployer.deploy(CryptoDollarStorageProxy)
 
   //link libraries to deployed contracts
-  deployer.link(DividendStorageProxy, [CryptoDollar, CryptoFiatHub]);
+  deployer.link(RewardsStorageProxy, [CryptoDollar, CryptoFiatHub, ProofRewards]);
   deployer.link(CryptoFiatStorageProxy, [CryptoDollar, CryptoFiatHub]);
   deployer.link(CryptoDollarStorageProxy, [CryptoDollar, CryptoFiatHub]);
-  deployer.link(SafeMath, [CryptoDollar, CryptoFiatHub]);
+  deployer.link(SafeMath, [CryptoDollar, CryptoFiatHub, ProofRewards]);
 
   deployer.deploy(Store)
     .then(() => { return deployer.deploy(ProofToken)})
     .then(() => { return deployer.deploy(CryptoDollar, Store.address)})
-    .then(() => { return deployer.deploy(CryptoFiatHub, CryptoDollar.address, Store.address, ProofToken.address )})
+    .then(() => { return deployer.deploy(ProofRewards, Store.address, ProofToken.address)})
+    .then(() => { return deployer.deploy(CryptoFiatHub, CryptoDollar.address, Store.address, ProofToken.address, ProofRewards.address )})
 };
