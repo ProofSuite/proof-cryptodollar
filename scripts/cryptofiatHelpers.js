5ef818a57ea3c5fa4e615e3475f5100a1e3d4517
David Van Isacker <davidvanisacker@gmail.com>
2018-02-09 14:21:00 +0900

    [#154904445] Added getState helper

--- scripts/cryptofiatHelpers.js
+++ scripts/cryptofiatHelpers.js
@@ -5,5 +5,20 @@ const getFee = (value, fee) => { return value * fee }
 const getOrderEtherValue = (value) => { return web3.fromWei(value - getFee(value, 0.01)) }
 const getOrderWeiValue = (value) => { return (value - getFee(value, 0.01)) }
 
+const getState = async(cryptoFiat) => {
+  let currentStateID = await cryptoFiat.currentState.call()
+  if (currentStateID.eq(1)) {
+    return 'UNPEGGED'
+  } else {
+    return 'PEGGED'
+  }
+}
 
-module.exports = { getBufferFee, applyFee, getFee, getOrderEtherValue, getOrderWeiValue }
+module.exports = {
+  getBufferFee,
+  applyFee,
+  getFee,
+  getOrderEtherValue,
+  getOrderWeiValue,
+  getState
+ }
