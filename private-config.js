e7bd627c1cb29d6da2e53460e56f05ddb6ea3677
David Van Isacker <davidvanisacker@gmail.com>
2018-02-20 19:05:31 +0900

    [#154516233] Fixed typo in private-config.js

--- private-config.js
+++ private-config.js
@@ -1,14 +1,14 @@
 let config = {
   rinkeby: {
-    keystore: process.env.RINKEBY_KEYSTORE
+    keystore: process.env.RINKEBY_KEYSTORE,
     password: process.env.RINKEBY_PASSWORD
   },
   ropsten: {
-    keystore: process.env.ROPSTEN_KEYSTORE
+    keystore: process.env.ROPSTEN_KEYSTORE,
     password: process.env.ROPSTEN_PASSWORD
   },
   ethereum: {
-    keystore: process.env.ETHEREUM_KEYSTORE
+    keystore: process.env.ETHEREUM_KEYSTORE,
     password: process.env.ETHEREUM_PASSWORD
   }
 }
