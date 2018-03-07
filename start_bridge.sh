7080e6c4450afd2a5c08c81930aba0a1ab1f0887
David Van Isacker <davidvanisacker@gmail.com>
2018-03-07 18:48:09 +0900

    [#155502959] Updated travis configuration and ethereum-bridge script

--- start_bridge.sh
+++ start_bridge.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-cd ./ethereum-bridge/
+cd $TRAVIS_BUILD_DIR/ethereum-bridge/
 npm install
-node bridge -H localhost:8545 -a 1 --dev
+node bridge -H localhost:8545 -a 1 --dev --update-ds
