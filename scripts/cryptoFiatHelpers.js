abb9532797b85d3d750b7c7baab8cac2dc299b5a
David Van Isacker <davidvanisacker@gmail.com>
2018-03-07 14:52:41 +0900

    [#155502959] Updated getState function helper

--- scripts/cryptoFiatHelpers.js
+++ scripts/cryptoFiatHelpers.js
@@ -6,8 +6,7 @@ const getFee = (value, fee) => { return value * fee }
 const getOrderEtherValue = (value) => { return web3.fromWei(value - getFee(value, 0.01)) }
 const getOrderWeiValue = (value) => { return (value - getFee(value, 0.01)) }
 
-const getState = async(cryptoFiat, priceFeed) => {
-  let exchangeRate = await priceFeed.exchangeRate.call()
+const getState = async(cryptoFiat, exchangeRate) => {
   let isPegged = await cryptoFiat.inState(0, exchangeRate)
   return (isPegged ? 'PEGGED' : 'UNPEGGED')
 }
