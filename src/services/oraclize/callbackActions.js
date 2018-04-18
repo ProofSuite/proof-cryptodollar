3c46b98294a2e75e93f8d5ed3cd991a520c11989
David Van Isacker <davidvanisacker@gmail.com>
2018-04-18 20:15:44 +0900

    [#156337274] Removed unneeded files

--- src/services/oraclize/callbackActions.js
+++
@@ -1,7 +0,0 @@
-import store from '../../redux-store'
-import CryptoFiatHub from '../../../build/contracts/CryptoFiatHub.json'
-import { getTruffleContractAddress } from '../../helpers/contractHelpers'
-
-export const createBuyCallbackListener = (receipt) => {
-  console.log(receipt)
-}
