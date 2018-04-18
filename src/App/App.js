3dc6db13f3f6f1c8dc3c2b345280b35bbc159cee
David Van Isacker <davidvanisacker@gmail.com>
2018-04-18 20:20:50 +0900

    [#156337274] Added wallet/provider settings layout

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import DEXLayout from '../layouts/DEXLayout'
 import CryptoFiatLayout from '../layouts/CryptoFiatLayout'
+import SettingsLayout from '../layouts/SettingsLayout'
+import TestLayout from '../layouts/TestLayout'
+import PropTypes from 'prop-types'
 
 import { hot } from 'react-hot-loader'
 import { Route, Switch, BrowserRouter } from 'react-router-dom'
@@ -11,9 +14,15 @@ import { Route, Switch, BrowserRouter } from 'react-router-dom'
 // const history = syncHistoryWithStore(createBrowserHistory(), store)
 
 import 'semantic-ui-css/semantic.min.css?global'
+import 'antd/dist/antd.css?global'
 import NavBar from '../components/common/NavBar'
 
 class App extends Component {
+
+  componentDidMount () {
+    this.props.initializeWeb3()
+    this.props.queryAccounts()
+  }
 
   render () {
     return (
@@ -23,11 +32,18 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={CryptoFiatLayout}/>
           <Route path='/dex' component={DEXLayout}/>
+          <Route path='/settings' component={SettingsLayout}/>
+          <Route path='/test' component={TestLayout} />
         </Switch>
         </div>
       </BrowserRouter>
     )
   }
+}
+
+App.propTypes = {
+  initializeWeb3: PropTypes.func,
+  queryAccounts: PropTypes.func
 }
 
 export default hot(module)(App)
